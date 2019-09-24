285c095234f930f0fd0e6ad7b13d690ba8f707e2
Derk Gorter <dgorter@deloitte.nl>
2019-09-24 15:21:08 +0200

    connect classes in main

--- getRichOrDieTrying.py
+++ getRichOrDieTrying.py
@@ -1,19 +1,30 @@
-from getRichOrDieTrying import User
-from getRichOrDieTrying import Market
+from getRichOrDieTrying.User import User
+from getRichOrDieTrying.Market import Market
+from getRichOrDieTrying.Portfolio import Portfolio
 
 
 def main():
 
+    #Initialize market
+    security_data = {"Apple": 239, "Shell": 100, "Google": 1200}
+    market = Market(security_data)
+
+    #create a portfolio with securities
+    securities = {"Apple": 2, "Shell": 1}
+
+    securities.keys()
+    portfolio = Portfolio(securities, 100000)
+    print("portfolio value")
+    print(portfolio.value(market))
+
+
     name = input("What is your name?")
     cash_deposit = input("How much cash would you like to deposit?")
+    user1 = User(name, int(cash_deposit))
 
-    user1 = User.User(name, int(cash_deposit))
 
     print(user1.name)
     print(user1.cash)
 
 
-
 main()
-
-
--- getRichOrDieTrying/Portfolio.py
+++ getRichOrDieTrying/Portfolio.py
@@ -1,16 +1,16 @@
 class Portfolio:
-    def __init__(self, fin_products, cash_account):
+    def __init__(self, fin_products, cash_acmount):
         self.fin_products = fin_products
-        self.cash_account = cash_account
+        self.cash_acmount = cash_acmount
 
     def update_portfolio(self, security, amount):
 
         #make a list/dictionary of the financial products in your portfolio and update when one is added through Order class
         print("update portfolio")
 
-    def show_value(self):
-        Market.value(self.fin_products)
-        print("Show value")
+    def value(self, market):
+        securities_value = market.value(self.fin_products)
+        return securities_value + self.cash_acmount
 
     def show_value_hist(self, date_range):
         print("Show value history")
