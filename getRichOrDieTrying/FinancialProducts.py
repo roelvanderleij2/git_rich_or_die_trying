dcb11e39f6e4c1e226849ae42ac167bbde19baf9
Tareq Salibi <tsalibi@deloitte.nl>
2019-09-24 14:42:35 +0200

    Add products

---
+++ getRichOrDieTrying/FinancialProducts.py
@@ -0,0 +1,21 @@
+class FinaincialProduct:
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+
+class Stock(FinaincialProduct):
+    def __int__(self):
+        pass
+
+
+class Bond(FinaincialProduct):
+    def __int__(self):
+        pass
+
+
+class Derivative(FinaincialProduct):
+    def __int__(self):
+        pass
