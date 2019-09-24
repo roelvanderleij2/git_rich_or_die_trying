0da0577cbd893cc52d0255fd3933609773dc5eda
rvanderleij <rvanderleij@deloitte.nl>
2019-09-24 17:36:05 +0200

    Added changes to User class

--- getRichOrDieTrying/User.py
+++ getRichOrDieTrying/User.py
@@ -1,11 +1,26 @@
+from getRichOrDieTrying.Order import Order
 from getRichOrDieTrying.Portfolio import Portfolio
 
 class User:
+    trade_list = []
     def __init__(self, name, start_cash):
         self.name = name
         self.portfolio = Portfolio(None, start_cash)
 
-
-
     def name(self):
         return self.name
+
+    def define_trades(self):
+        continue_statement = True
+        new_trades = []
+        while continue_statement == True:
+            ticker = input("Provide the ticker of the security you would like to trade?")
+            order_type = input("What type of order would you like to do (e.g. Buy, Sell, Limit, Stop-Loss)?")
+            amount = input("How much of this security would you like to trade?")
+            trade_dict = {'Ticker': ticker, 'Order Type': order_type, 'Amount': amount}
+            new_trades.append(dict(trade_dict))
+            continue_statement = input("Would you like to add another trade (True/False)?")
+
+        self.trade_list.append(new_trades)
+
+   #def execute_trades(self, trade_list):
