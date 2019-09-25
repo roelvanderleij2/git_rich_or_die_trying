7935536328937741ded1e06f6cc42656c03c4d1a
Tareq Salibi <tsalibi@deloitte.nl>
2019-09-25 09:39:51 +0200

    Flask update

--- getRichOrDieTrying.py
+++ getRichOrDieTrying.py
@@ -1,11 +1,26 @@
 from getRichOrDieTrying.User import User
 from getRichOrDieTrying.Market import Market
+from flask import Flask, render_template, request
 import datetime as dt
+from flask import Flask, render_template, request
+import getRichOrDieTrying.Order
+
+app = Flask(__name__)
+
+
+@app.route("/profile/<username>/<int:id_number>",
+           methods=["GET", "POST"])  # the user will ask for this web-page where the user should enter the variable
+# username and id_number
+def profile(username, id_number):
+    return render_template("profile.html", username=username, id_number=id_number)
+
+
+#if __name__ == "__main__":
+#    app.run(debug=True)
 
 
 def main():
-
-    #Initialize market
+    # Initialize market
     market = Market()
 
     print("Welcome to Git Rich Or Die Trying investment platform")
@@ -13,9 +28,9 @@ def main():
     cash_deposit = int(input("How much cash would you like to deposit in your account?\n"))
     user1 = User(name, cash_deposit)
 
-    #acquired_securities = user.create_order(buy, "Google", 1)
+    # acquired_securities = user.create_order(buy, "Google", 1)
 
-    #create a portfolio with securities
+    # create a portfolio with securities
     user1.define_trades()
     print(user1.trade_list)
 
@@ -24,6 +39,7 @@ def main():
     user1.portfolio.update_portfolio(acquired_securities)
 
     print("portfolio value:")
-    print(user1.portfolio.value(market, dt.datetime(2019,9,23)))
+    print(user1.portfolio.value(market, dt.datetime(2019, 9, 23)))
+
 
 main()
